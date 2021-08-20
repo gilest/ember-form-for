37778066ad253239404e408e4711a21562b0475b
Nicklas Andersson <nicklas.andersson@ecraft.com>
2021-08-20 17:29:28 +0200

    Try to update ember

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,12 @@
-import { start } from 'ember-cli-qunit';
-import resolver from './helpers/resolver';
-import { setResolver } from '@ember/test-helpers';
+import Application from 'dummy/app';
+import config from 'dummy/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
 
-setResolver(resolver);
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
 start();
